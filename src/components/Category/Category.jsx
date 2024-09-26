bb5375c5be831ec3566a96853cd5f837a980a180
VietAnh30 <vietanhdang487@gmail.com>
2024-09-26 15:44:00 +0700

    fix

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 function Category({ category }) {
     return (
-        <div className="border px-14 py-6 mx-auto mr-7 h-20">
+        <div className="border px-14 py-6 mx-auto mr-7 h-40">
             <div className='flex justify-center'>
                 <img src={category.img || phone} alt={category.name} className="w-16 h-16 object-contain" />
             </div>
-            <div className="text-base font-normal flex justify-center mt-4">
+            <div className="text-sm font-normal flex text-center justify-center mt-4">
                 {category.name}
             </div>
         </div>
